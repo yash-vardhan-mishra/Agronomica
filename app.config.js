c7558de628f7b10b45611cc73cf4da9181663f02
Yash Vardhan <yash-vardhan@hotmail.com>
2024-10-06 06:24:21 +1300

    renamed app and repo

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = ({ config }) => {
   return {
     ...config,
     ios: {
-      bundleIdentifier: "com.agronomica.plus",
+      bundleIdentifier: "com.agronomica.app",
       config: {
         googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
       }
