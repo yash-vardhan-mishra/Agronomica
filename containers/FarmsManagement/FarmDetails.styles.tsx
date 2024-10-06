98cfcb9785e7667bb9b29d51fb12966093e24fd5
Yash Vardhan <yash-vardhan@hotmail.com>
2024-10-07 12:09:37 +1300

    fixed farm details UI

--- containers/FarmsManagement/FarmDetails.styles.tsx
+++ containers/FarmsManagement/FarmDetails.styles.tsx
@@ -5,7 +5,7 @@ import Dimensions from "../../constants/Dimensions";
 const styles = StyleSheet.create({
     container: { flex: 1, backgroundColor: Colors.white },
     inputContainer: { marginTop: 20 },
-    detailsContainer: { justifyContent: 'space-between', paddingBottom: 20, paddingHorizontal: 20 },
+    detailsContainer: { justifyContent: 'space-between', padding: 20 },
     dropdownContainer: {
         marginBottom: 16,
         width: '100%',
